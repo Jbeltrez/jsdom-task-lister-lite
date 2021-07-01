df710f3e1beaf5827c08238feb6d53e79816be47
Jbeltrez <beltrezjohn@gmail.com>
2021-07-01 11:11:29 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+// listen for click event to grab the value submitted in 
+// form field and append it to a list 
+let formValue = document.getElementById("new-task-description"); 
+let tasks = document.getElementById("tasks"); 
+document.addEventListener("submit", (e) => {
+  e.preventDefault(); 
+  console.log(formValue.value)
+  let task = formValue.value 
+  appendTask(task); 
+  formValue.value = ""; 
+
+})
+
+function appendTask(task) {
+  let li = document.createElement("li");
+  li.innerHTML = task;
+  tasks.append(li); 
+}
